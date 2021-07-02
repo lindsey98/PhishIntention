574daadf841731a16584852495d45c86ac0b8ec8
ruofanliu <kelseyliu1988@gmail.com>
2021-07-02 10:55:59 +0800

    fix bug

--- schedule_run.py
+++ schedule_run.py
@@ -4,22 +4,26 @@ import datetime
 import sys
 from datetime import datetime, timedelta, time
 import argparse
-from phishintention_main import *
-# from phishpedia_main import *
+# from phishintention_main import *
+from phishpedia_main import *
 import os
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 
 if __name__ == '__main__':
 
-    print('Today is:', datetime.today().strftime('%Y-%m-%d'))
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-f', "--folder", default='E:\\screenshots_rf\\{}'.format(datetime.today().strftime('%Y-%m-%d')), help='Input folder path to parse')
-    parser.add_argument('-r', "--results", default=datetime.today().strftime('%Y-%m-%d')+'.txt', help='Input results file name')
-    args = parser.parse_args()
-    print(args)
-
     while True:
-        runit(args.folder, args.results)
+        print('Today is:', datetime.today().strftime('%Y-%m-%d'))
+
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-f', "--folder",
+                            default='E:\\screenshots_rf\\{}'.format(datetime.today().strftime('%Y-%m-%d')),
+                            help='Input folder path to parse')
+        parser.add_argument('-r', "--results", default=datetime.today().strftime('%Y-%m-%d') + '.txt',
+                            help='Input results file name')
+        args = parser.parse_args()
+        print(args)
+        # runit(args.folder, args.results)
+        runit_pedia(args.folder, args.results)
         print('Process finish')
 
