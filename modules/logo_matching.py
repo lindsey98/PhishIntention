86e7d1924409494e9c160b3994f512c7b54d0aec
lindsey98 <51521323+lindsey98@users.noreply.github.com>
2025-04-02 08:35:07 +0800

    #issuecomment-2770923614
    #issuecomment-2770923614

--- modules/logo_matching.py
+++ modules/logo_matching.py
@@ -13,6 +13,269 @@ from tqdm import tqdm
 from tldextract import tldextract
 import pickle
 
+COUNTRY_TLDs = [
+    ".af",
+    ".ax",
+    ".al",
+    ".dz",
+    ".as",
+    ".ad",
+    ".ao",
+    ".ai",
+    ".aq",
+    ".ag",
+    ".ar",
+    ".am",
+    ".aw",
+    ".ac",
+    ".au",
+    ".at",
+    ".az",
+    ".bs",
+    ".bh",
+    ".bd",
+    ".bb",
+    ".eus",
+    ".by",
+    ".be",
+    ".bz",
+    ".bj",
+    ".bm",
+    ".bt",
+    ".bo",
+    ".bq",".an",".nl",
+    ".ba",
+    ".bw",
+    ".bv",
+    ".br",
+    ".io",
+    ".vg",
+    ".bn",
+    ".bg",
+    ".bf",
+    ".mm",
+    ".bi",
+    ".kh",
+    ".cm",
+    ".ca",
+    ".cv",
+    ".cat",
+    ".ky",
+    ".cf",
+    ".td",
+    ".cl",
+    ".cn",
+    ".cx",
+    ".cc",
+    ".co",
+    ".km",
+    ".cd",
+    ".cg",
+    ".ck",
+    ".cr",
+    ".ci",
+    ".hr",
+    ".cu",
+    ".cw",
+    ".cy",
+    ".cz",
+    ".dk",
+    ".dj",
+    ".dm",
+    ".do",
+    ".tl",".tp",
+    ".ec",
+    ".eg",
+    ".sv",
+    ".gq",
+    ".er",
+    ".ee",
+    ".et",
+    ".eu",
+    ".fk",
+    ".fo",
+    ".fm",
+    ".fj",
+    ".fi",
+    ".fr",
+    ".gf",
+    ".pf",
+    ".tf",
+    ".ga",
+    ".gal",
+    ".gm",
+    ".ps",
+    ".ge",
+    ".de",
+    ".gh",
+    ".gi",
+    ".gr",
+    ".gl",
+    ".gd",
+    ".gp",
+    ".gu",
+    ".gt",
+    ".gg",
+    ".gn",
+    ".gw",
+    ".gy",
+    ".ht",
+    ".hm",
+    ".hn",
+    ".hk",
+    ".hu",
+    ".is",
+    ".in",
+    ".id",
+    ".ir",
+    ".iq",
+    ".ie",
+    ".im",
+    ".il",
+    ".it",
+    ".jm",
+    ".jp",
+    ".je",
+    ".jo",
+    ".kz",
+    ".ke",
+    ".ki",
+    ".kw",
+    ".kg",
+    ".la",
+    ".lv",
+    ".lb",
+    ".ls",
+    ".lr",
+    ".ly",
+    ".li",
+    ".lt",
+    ".lu",
+    ".mo",
+    ".mk",
+    ".mg",
+    ".mw",
+    ".my",
+    ".mv",
+    ".ml",
+    ".mt",
+    ".mh",
+    ".mq",
+    ".mr",
+    ".mu",
+    ".yt",
+    ".mx",
+    ".md",
+    ".mc",
+    ".mn",
+    ".me",
+    ".ms",
+    ".ma",
+    ".mz",
+    ".mm",
+    ".na",
+    ".nr",
+    ".np",
+    ".nl",
+    ".nc",
+    ".nz",
+    ".ni",
+    ".ne",
+    ".ng",
+    ".nu",
+    ".nf",
+    ".nc",".tr",
+    ".kp",
+    ".mp",
+    ".no",
+    ".om",
+    ".pk",
+    ".pw",
+    ".ps",
+    ".pa",
+    ".pg",
+    ".py",
+    ".pe",
+    ".ph",
+    ".pn",
+    ".pl",
+    ".pt",
+    ".pr",
+    ".qa",
+    ".ro",
+    ".ru",
+    ".rw",
+    ".re",
+    ".bq",".an",
+    ".bl",".gp",".fr",
+    ".sh",
+    ".kn",
+    ".lc",
+    ".mf",".gp",".fr",
+    ".pm",
+    ".vc",
+    ".ws",
+    ".sm",
+    ".st",
+    ".sa",
+    ".sn",
+    ".rs",
+    ".sc",
+    ".sl",
+    ".sg",
+    ".bq",".an",".nl",
+    ".sx",".an",
+    ".sk",
+    ".si",
+    ".sb",
+    ".so",
+    ".so",
+    ".za",
+    ".gs",
+    ".kr",
+    ".ss",
+    ".es",
+    ".lk",
+    ".sd",
+    ".sr",
+    ".sj",
+    ".sz",
+    ".se",
+    ".ch",
+    ".sy",
+    ".tw",
+    ".tj",
+    ".tz",
+    ".th",
+    ".tg",
+    ".tk",
+    ".to",
+    ".tt",
+    ".tn",
+    ".tr",
+    ".tm",
+    ".tc",
+    ".tv",
+    ".ug",
+    ".ua",
+    ".ae",
+    ".uk",
+    ".us",
+    ".vi",
+    ".uy",
+    ".uz",
+    ".vu",
+    ".va",
+    ".ve",
+    ".vn",
+    ".wf",
+    ".eh",
+    ".ma",
+    ".ye",
+    ".zm",
+    ".zw"
+]
+
 class DataInfo(object):
     """
     Save the info about the dataset.
@@ -300,7 +563,12 @@ def check_domain_brand_inconsistency(logo_boxes,
 
     # look at boxes for logo class only
     print('number of logo boxes:', len(logo_boxes))
+    suffix_part = '.'+ tldextract.extract(url).suffix
+    domain_part = tldextract.extract(url).domain
+    extracted_domain = domain_part + suffix_part
+                                         
     matched_target, matched_domain, matched_coord, this_conf = None, None, None, None
+
 
     # run logo matcher
     if len(logo_boxes) > 0:
@@ -313,18 +581,34 @@ def check_domain_brand_inconsistency(logo_boxes,
                                                                   shot_path, bbox, t_s=ts, grayscale=False)
 
             # domain matcher to avoid FP
-            if matched_target is not None:
+            # if matched_target is not None:
+            #     matched_coord = coord
+            #     # if tldextract.extract(url).domain+ '.'+tldextract.extract(url).suffix not in matched_domain:
+            #     if tldextract.extract(url).domain not in matched_domain:
+            #         # avoid fp due to godaddy domain parking, ignore webmail provider (ambiguous)
+            #         if matched_target == 'GoDaddy' or matched_target == "Webmail Provider" or matched_target == "Government of the United Kingdom":
+            #             matched_target = None  # ignore the prediction
+            #             matched_domain = None  # ignore the prediction
+            #     else:  # benign, real target
+            #         matched_target = None  # ignore the prediction
+            #         matched_domain = None  # ignore the prediction
+            #     break  # break if target is matched
+            # break  # only look at 1st logo
+            if (matched_target is not None) and (matched_domain is not None):
                 matched_coord = coord
-                # if tldextract.extract(url).domain+ '.'+tldextract.extract(url).suffix not in matched_domain:
-                if tldextract.extract(url).domain not in matched_domain:
-                    # avoid fp due to godaddy domain parking, ignore webmail provider (ambiguous)
-                    if matched_target == 'GoDaddy' or matched_target == "Webmail Provider" or matched_target == "Government of the United Kingdom":
-                        matched_target = None  # ignore the prediction
-                        matched_domain = None  # ignore the prediction
-                else:  # benign, real target
-                    matched_target = None  # ignore the prediction
-                    matched_domain = None  # ignore the prediction
-                break  # break if target is matched
+                matched_domain_parts = [tldextract.extract(x).domain for x in matched_domain]
+                matched_suffix_parts = [tldextract.extract(x).suffix for x in matched_domain]
+                
+                # If the webpage domain exactly aligns with the target website's domain => Benign
+                if extracted_domain in matched_domain:
+                    matched_target, matched_domain = None, None  # Clear if domains are consistent
+                elif domain_part in matched_domain_parts: # # elIf only the 2nd-level-domains align, and the tld is regional  => Benign
+                    if "." + suffix_part.split('.')[-1] in COUNTRY_TLDs:
+                        matched_target, matched_domain = None, None
+                    else:
+                        break # Inconsistent domain found, break the loop
+                else:
+                    break  # Inconsistent domain found, break the loop
             break  # only look at 1st logo
 
     return brand_converter(matched_target), matched_domain, matched_coord, this_conf
