c4b1410d2edab1efdee173f0ad5367765e2b8a83
syy-yoyo <syysyysyy1010@sjtu.edu.cn>
2026-01-03 00:19:24 +0800

    提供了自动判断平台与cuda配置进行pytorch与detectron2安装的脚本

--- setup.sh
+++ setup.sh
@@ -6,19 +6,10 @@ IFS=$'\n\t'
 # # Install Detectron2
 # pixi run pip install --extra-index-url https://miropsota.github.io/torch_packages_builder detectron2==0.6+18f6958pt2.8.0cpu
 
-# Check if cpu flag is provided
-if [[ "$1" == "cpu" ]]; then
-  echo "Installing PyTorch and Detectron2 (CPU version)..."
-  pixi run python auto_install_detectron2.py --cpu --auto
-else
-  # Automatically install PyTorch and Detectron2
-  # Automatically select CPU or CUDA version based on whether NVIDIA GPU is available
-  echo "Installing PyTorch and Detectron2 (auto-detecting GPU)..."
-  pixi run python auto_install_detectron2.py
-fi
-
-
-
+# Automatically install PyTorch and Detectron2
+# Automatically select CPU or CUDA version based on whether NVIDIA GPU is available
+echo "Installing PyTorch and Detectron2 (auto-detecting GPU)..."
+pixi run python auto_install_detectron2.py
 
 # Set up model directory
 FILEDIR="$(pwd)"
